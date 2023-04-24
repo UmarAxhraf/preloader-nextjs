ccb0253bf1619b273c65bb2356f2803ad80cfd86
Umar Ashraf <umarashraf7827@gmail.com>
2023-04-24 11:11:31 +0500

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import LoadingScreen from "../components/loadingscreen";
+
+function MyApp({ Component, pageProps }) {
+  const [loading, setLoading] = React.useState(false);
+
+  React.useEffect(() => {
+    setLoading(true);
+    setTimeout(() => setLoading(false), 3000);
+  }, []);
+}
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>{!loading ? <Component {...pageProps} /> : <LoadingScreen />}</>;
 }
